0f5d58406b0bebcf8afef176b10a3a9aa0bb53c5
Scott.Stanchfield <Scott.Stanchfield@88cfd0be-b251-0410-a78a-4b8c7298577f>
2008-07-10 23:04:30 +0000

    initial checkin
    git-svn-id: http://javadude.googlecode.com/svn/trunk@16 88cfd0be-b251-0410-a78a-4b8c7298577f

---
+++ com.javadude.common/src/com/javadude/common/PluginUtil.java
@@ -0,0 +1,188 @@
+/*******************************************************************************
+ *  Copyright 2008 Scott Stanchfield.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *******************************************************************************/
+package com.javadude.common;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+
+public class PluginUtil {
+    private ILog log_;
+    private String pluginName_;
+
+    public PluginUtil(String pluginName, ILog log) {
+        pluginName_ = pluginName;
+        log_ = log;
+    }
+
+    /**
+     * Log an error.
+     * @param message the message
+     */
+    public void error(int code, String message) {
+        error(code, message, null);
+    }
+    /**
+     * Log a warning.
+     * @param message the message
+     */
+    public void warning(int code, String message) {
+        warning(code, message, null);
+    }
+    /**
+     * Log an info message.
+     * @param message the message
+     */
+    public void info(int code, String message) {
+        info(code, message, null);
+    }
+    /**
+     * Log an error.
+     * @param message the message
+     * @param throwable an optional exception
+     */
+    public void error(int code, final String message, Throwable throwable) {
+        IStatus status = new Status(IStatus.ERROR, pluginName_, code, message, throwable);
+        log_.log(status);
+        Display.getDefault().asyncExec(new Runnable() {
+            public void run() {
+                MessageDialog.openError(null, "Error setting Hillcrest Classpath", message);
+            }});
+    }
+    /**
+     * Log a warning.
+     * @param message the message
+     * @param throwable an optional exception
+     */
+    public void warning(int code, final String message, Throwable throwable) {
+        IStatus status = new Status(IStatus.WARNING, pluginName_, code, message, throwable);
+        log_.log(status);
+        Display.getDefault().asyncExec(new Runnable() {
+            public void run() {
+                MessageDialog.openWarning(null, "Warning setting Hillcrest Classpath", message);
+            }});
+    }
+    /**
+     * Log an info message.
+     * @param message the message
+     * @param throwable an optional exception
+     */
+    public void info(int code, String message, Throwable throwable) {
+        IStatus status = new Status(IStatus.INFO, pluginName_, code, message, throwable);
+        log_.log(status);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void toggleNature(String natureId, ISelection selection) {
+        if (selection instanceof IStructuredSelection) {
+            List<String> readOnlyProblemFiles = new ArrayList<String>();
+            for (Iterator<Object> it = ((IStructuredSelection) selection).iterator(); it.hasNext();) {
+                Object element = it.next();
+                IProject project = null;
+                if (element instanceof IProject) {
+                    project = (IProject) element;
+                } else if (element instanceof IAdaptable) {
+                    project = (IProject) ((IAdaptable) element).getAdapter(IProject.class);
+                }
+                if (project != null) {
+                    toggleNature(natureId, project, readOnlyProblemFiles);
+                }
+            }
+            if (!readOnlyProblemFiles.isEmpty()) {
+                String message = "The following files are read-only or do not exist and their projects cannot be converted to/from Hillcrest Adapter Projects:";
+                for (String readOnlyFile : readOnlyProblemFiles) {
+                    message += '\n' + readOnlyFile;
+                }
+                error(1001, message);
+            }
+        }
+    }
+
+    private void toggleNature(String natureId, IProject project, List<String> readOnlyProblemFiles) {
+        try {
+            // check if the project's .classpath and .project files are writeable
+            IFile file = project.getFile(".classpath");
+            if (!file.exists() || file.isReadOnly()) {
+                readOnlyProblemFiles.add(file.getLocation().toOSString());
+            }
+            file = project.getFile(".project");
+            if (!file.exists() || file.isReadOnly()) {
+                readOnlyProblemFiles.add(file.getLocation().toOSString());
+            }
+
+            IProjectDescription description = project.getDescription();
+            String[] natures = description.getNatureIds();
+
+            for (int i = 0; i < natures.length; ++i) {
+                if (natureId.equals(natures[i])) {
+                    // Remove the nature
+                    String[] newNatures = new String[natures.length - 1];
+                    System.arraycopy(natures, 0, newNatures, 0, i);
+                    System.arraycopy(natures, i + 1, newNatures, i,
+                            natures.length - i - 1);
+                    description.setNatureIds(newNatures);
+                    project.setDescription(description, null);
+                    return;
+                }
+            }
+
+            // Add the nature
+            String[] newNatures = new String[natures.length + 1];
+            System.arraycopy(natures, 0, newNatures, 1, natures.length);
+            newNatures[0] = natureId;
+            description.setNatureIds(newNatures);
+            project.setDescription(description, null);
+//          PlatformUI.getWorkbench().getDecoratorManager().update("com.hcrest.classpath.decorator");
+        } catch (CoreException e) {
+            // do nothing
+        }
+    }
+    public boolean hasNature(Object resource, String natureId) {
+        if (resource instanceof IJavaProject) {
+            resource = ((IJavaProject) resource).getProject();
+        }
+        if (resource instanceof IProject) {
+            return hasNature((IProject) resource, natureId);
+        }
+        return false;
+    }
+
+    public boolean hasNature(IProject aProject, String natureId) {
+        boolean hasNature;
+        try {
+            hasNature = aProject.isOpen() && aProject.hasNature(natureId);
+        } catch (CoreException e) {
+            error(1002, "Error checking nature", e);
+            hasNature = false;
+        }
+        return hasNature;
+    }
+
+}
