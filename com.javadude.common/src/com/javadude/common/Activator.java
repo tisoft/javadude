cd21f75a9d3c10b75b4208d720bad3e8852d38df
scott@javadude.com <scott@javadude.com@88cfd0be-b251-0410-a78a-4b8c7298577f>
2008-09-16 01:52:45 +0000

    change license to EPL 1.0
    git-svn-id: http://javadude.googlecode.com/svn/trunk@170 88cfd0be-b251-0410-a78a-4b8c7298577f

--- com.javadude.common/src/com/javadude/common/Activator.java
+++ com.javadude.common/src/com/javadude/common/Activator.java
@@ -1,17 +1,9 @@
 /*******************************************************************************
- *  Copyright 2008 Scott Stanchfield.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (c) 2008 Scott Stanchfield
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 package com.javadude.common;
 
@@ -33,7 +25,7 @@ public class Activator extends AbstractUIPlugin {
      * The constructor
      */
     public Activator() {
-        plugin = this;
+        Activator.plugin = this;
     }
 
     @Override
@@ -43,7 +35,7 @@ public class Activator extends AbstractUIPlugin {
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        plugin = null;
+        Activator.plugin = null;
         super.stop(context);
     }
 
@@ -53,7 +45,7 @@ public class Activator extends AbstractUIPlugin {
      * @return the shared instance
      */
     public static Activator getDefault() {
-        return plugin;
+        return Activator.plugin;
     }
 
 }
